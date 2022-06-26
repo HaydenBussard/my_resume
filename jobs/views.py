fb2a043231b272e7d47832fcd96b26a284c246fc
HaydenBussard <haydenbussard@outlook.com>
2022-06-26 09:11:50 -0500

    ..

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,4 @@
+import requests
 from webbrowser import get
 from django.shortcuts import render, get_object_or_404
 from .models import Job
@@ -11,6 +12,11 @@ def detail(request, job_id):
     job_detail = get_object_or_404(Job, pk=job_id)
     return render(request, 'jobs/detail.html', {'job':job_detail})
 
+# def index(request):
+#     jobs = Job.objects
+#     return render(request, 'index.html', {'index':index})
+
 def index(request):
-    jobs = Job.objects
-    return render(request, 'index.html', {'index':index})
+    r = requests.get('https://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
